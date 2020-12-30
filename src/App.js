a033bac37b237e6acf8bcaa12288bc320b81646b
Winston Brown <johnbaker11568@gmail.com>
2020-12-29 19:26:59 -0500

    Learned hot to do fetch in react

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import "./App.css";
-const computersFormat = [
-  {
-    id:1,
-    imagePath : "https://images-na.ssl-images-amazon.com/images/I/51I3UjD-Q1L.jpg",
-    description:"Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black",
-    author:"by Acer"
-  },
-  {
-    id:2,
-    imagePath : "https://images-na.ssl-images-amazon.com/images/I/31jBba7+ySL.jpg",
-    description:"Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PS4, & Xbox - 1-year Rescue Service (STGX2000400)",
-    author:"by Seagate"
-  },
-  {
-    id:3,
-    imagePath : "https://images-na.ssl-images-amazon.com/images/I/41QztBTooxL.jpg",
-    description:"RUNMUS Gaming Headset Xbox One Headset with 7.1 Surround Sound, PS4 Headset with Noise Canceling Mic & LED Light, Compatible with PC, PS4",
-    author:"by RUNMUS"
-  }
-  
-]
+
  function App() {
   return (
    <>
- {computersFormat.map(computersSection =>  < ComputerList computers={computersSection}/>)}
+   <MoviesList />
    </>
   )
 }
-function ComputerList (props) {
-  const {imagePath,author,description,id} = props.computers
+function MoviesList() {
+  const [movies,setMovies] = useState([]);
+
+  useEffect(() => {
+  fetch("http://api.tvmaze.com/search/shows?q=teen")
+  .then(res => res.json())
+  .then(res => {
+    setMovies(res)
+  })
+  .catch(err => console.log(err.message))
+  }, [])
+  
 return(
  <>
- <section id={id} className="section-computers">
-  <img src={imagePath} />
-  <p>{description}</p>
-  <span>{author}</span>
- </section>
+  {movies.map(movie => < Movie  movies={movie}/>)
+}
  </>
 )
 }
+function Movie (props) {
+  const {image,genres,status,id,name} = props.movies.show;
+  let path;
+  for( const imagePath in image) if(imagePath === "medium") path = image[imagePath]
+  return (
+  <>
+  <section key={id}>
+    <h2>{name}</h2>
+    <img src={path} />
+    <ul>
+      {genres.map(genre => <li>{genre}</li>)}
+    </ul>
+    <span>{status}</span>
+  </section>
+
+  </>
+  )
+}
+
 export default App;
